4a934098c8281bd690c60b46b282240cc317a378
田贺锁 <tianhesuo@ifuncun.cn>
2022-01-04 10:04:37 +0800

    将地名人名作为停用词

--- Kmeans_cluster.py
+++ Kmeans_cluster.py
@@ -4,7 +4,7 @@
 Description: 使用KMEANS方法进行聚类分析
 Author: Tian Hesuo
 Date: 2021-12-27 14:22:06
-LastEditTime: 2021-12-31 14:20:52
+LastEditTime: 2022-01-04 10:03:34
 '''
 '''
 
@@ -33,17 +33,16 @@ LastEditTime: 2021-12-31 14:20:52
 '''
 
 import json
-import jieba
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
-import random
+import jieba
+import jieba.posseg as pseg
+jieba.enable_paddle()
 import warnings
-
 warnings.filterwarnings("ignore")
-
 
 def stopwords(stopwords_path):
     CUSTOM_STOP_WORDS = ["\u3000", "\n", " "]
@@ -54,45 +53,66 @@ def stopwords(stopwords_path):
             sw.append(word)
     return sw
 
+def get_extra_stopwords(text):
+    extra_stopwords = []  # 人名列表
+    word_list = jieba.lcut(text)
+    for word in word_list:
+        if len(word)==1:  # 不加判断会爆
+            continue
+        words = pseg.lcut(word, use_paddle=True)  # paddle模式
+        print(list(words))
+        word, flag = list(words)[0]
+        if flag=='LOC' or flag == "PER":  # 这里写成LOC是地名
+            extra_stopwords.append(word)
+    extra_stopwords = list(set(extra_stopwords))
+    return extra_stopwords
+
 
 # 通过所选文本构建语料库
-def build_corpus(corpus_path, stopwords):
+def build_corpus(corpus_path, stopwords):      
     corpus = []
     title_list = []
     with open(file=corpus_path, mode="r", encoding="utf8") as f:
         corpus_data = [json.loads(text) for text in f.readlines()]
-
-    # # # 随机选取
+        
+    # 随机选取
     # random_data = []
     # for i in random.sample(range(0, 87984), 10000):
     #     random_data.append(corpus_data[i])
     # corpus_data = random_data
+    
     jieba.load_userdict("data/userdict.txt")
+
+    extra_stopwords = []
     for text in corpus_data:
         cluster_text = " ".join(jieba.lcut(text["cluster_text"]))
         title_list.append(text["title"])
         corpus.append(cluster_text)
+        extra_stopwords.append(get_extra_stopwords(text["cluster_text"]))
+    # 额外补充地名、人名作为停用词
+    stopwords.extend(extra_stopwords)
+        
     vectorizer = CountVectorizer(stop_words=stopwords, max_df=0.8)
     vectorizer_fit = vectorizer.fit_transform(corpus)
     word_dict = {}
+    
     for index, word in enumerate(vectorizer.get_feature_names()):
         word_dict[index] = word
     print("关键词的数量为{}".format(len(word_dict)))
-
+    
     # print(vectorizer.vocabulary_)
     # print(vectorizer_fit.toarray())
     tfidf_ = TfidfTransformer()
     tfidf_fit = tfidf_.fit_transform(vectorizer_fit)
     return tfidf_fit, word_dict, title_list
-
-
+    
 def Kmeans_cluster(tfidf_fit, word_dict, title_list, cluster_words_path):
     # kmeans.labels_   # 每个样本所属的簇
     # kmeans.inertia_  # 用来评估簇的个数是否合适，距离越小说明簇分的越好，选取临界点的簇个数
     # (kmeans.cluster_centers_.argsort()[:, ::-1]) # 找出关键词
     # 找出聚类的中心
+    
     nums = []
-    silhouette_score_ = []
     inertia_score_ = []
     pred_ = []
     kmeans_reslut = []
@@ -101,63 +121,63 @@ def Kmeans_cluster(tfidf_fit, word_dict, title_list, cluster_words_path):
     for k in range(10, 15):
         kmeans = KMeans(n_clusters=k, random_state=9, n_jobs=6)
         pred = kmeans.fit_predict(tfidf_fit)
-        order_centroids = kmeans.cluster_centers_.argsort()[:, ::-1]
+        order_centroids = kmeans.cluster_centers_.argsort()[:, ::-1]  
         # silhouette = silhouette_score(tfidf_fit, pred)
         # print("silhouette_score {} for cluster {}".format(silhouette, k))
         inertia = kmeans.inertia_
         print("inertia_score {} for cluster {}".format(inertia, k))
-
+        
         nums.append(k)
         # silhouette_score_.append(silhouette)
         inertia_score_.append(inertia)
         pred_.append(pred.tolist())
-
+        
         # 添加聚类的词语
-        f_clusterwords.write("聚类数量为" + str(k) + ".................")
+        f_clusterwords.write("聚类数量为"+str(k)+".................")
         f_clusterwords.write("\n")
         cluster = 0
-        for ind in order_centroids:  # 每个聚类选 50 个词
-            words = []
+        for ind in order_centroids: # 每个聚类选 50 个词
+            words=[]
             for index in ind[:20]:
                 words.append(word_dict[index])
-            f_clusterwords.write(str(cluster) + '\t' + ','.join(words) + '\n')
-            cluster += 1
+            f_clusterwords.write(str(cluster)+'\t'+','.join(words)+'\n')
+            cluster+=1
         f_clusterwords.write("\n")
     f_clusterwords.close()
-
+    
     # 转成字典方便查找
     # dict_silhouette_score = dict(zip(nums, silhouette_score_))
-    dict_inertia_score = dict(zip(nums, inertia_score_))
+    dict_inertia_score = dict(zip(nums,inertia_score_))
     kmeans_reslut = []
     for i, pre in enumerate(pred_):
         cluster_mark = i + 10
-        kmeans_dict = {cluster_mark: []}  # {cluster_mark:[{0:[]}, {1:[]}]}
+        kmeans_dict = {cluster_mark:[]}     # {cluster_mark:[{0:[]}, {1:[]}]}
         for j in range(cluster_mark):
-            kmeans_dict[cluster_mark].append({j: []})
+            kmeans_dict[cluster_mark].append({j:[]})
         for k, text in enumerate(pre):
             kmeans_dict[cluster_mark][text][text].append(title_list[k])
         # kmeans_dict["dict_silhouette_score"] = dict_silhouette_score
         kmeans_dict["dict_inertia_score"] = dict_inertia_score
         kmeans_reslut.append(kmeans_dict)
-
-    with open("outputs/Kmeans/Kmeans_result.json", mode="w", encoding="utf8") as f:
+    
+    with open("outputs/Kmeans/Kmeans_result.json", mode = "w", encoding="utf8") as f:
         for text in kmeans_reslut:
             f.write(json.dumps(text, ensure_ascii=False))
             f.write("\n")
-
-    # plt.figure()
-    # plt.plot(nums, silhouette_score_)
-    # plt.xlabel("kmeans-k")
-    # plt.ylabel("silhouette_score")
-    # plt.title("matrix")
+    
+    # plt.figure()  
+    # plt.plot(nums, silhouette_score_)  
+    # plt.xlabel("kmeans-k")  
+    # plt.ylabel("silhouette_score")  
+    # plt.title("matrix")  
     # plt.savefig("outputs/Kmeans/silhouette_score.png")
     # plt.show()
 
-    plt.figure()
-    plt.plot(nums, inertia_score_)
+    plt.figure()  
+    plt.plot(nums,inertia_score_)  
     plt.xlabel("kmeans-k")
-    plt.ylabel("inertia_score(sum of squared)")
-    plt.title("matrix")
+    plt.ylabel("inertia_score(sum of squared)")  
+    plt.title("matrix")  
     plt.savefig("outputs/Kmeans/inertia_score.png")
     plt.show()
 
@@ -169,4 +189,4 @@ if __name__ == "__main__":
     sw = stopwords(stopwords_path)
     tfidf_fit, word_dict, title_list = build_corpus(corpus_path, sw)
     Kmeans_cluster(tfidf_fit, word_dict, title_list, cluster_words_path)
-
+    
--- LDA_subject.py
+++ LDA_subject.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 '''
-Description:
+Description: 
 Author: Tian Hesuo
 Date: 2021-12-24 14:47:00
-LastEditTime: 2021-12-31 16:07:00
+LastEditTime: 2022-01-04 10:03:50
 '''
 
 import jieba
+import jieba.posseg as pseg
+from numpy.lib.function_base import extract
+jieba.enable_paddle()
 import json
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 import numpy as np
 import warnings
-
 warnings.filterwarnings("ignore")
 import multiprocessing
 import random
@@ -21,7 +23,6 @@ import os
 
 # 常量设置
 THEME_NUMS = 10
-
 
 def stopwords_func(stopwords_path):
     CUSTOM_STOP_WORDS = ["\u3000", "\n", " "]
@@ -32,6 +33,19 @@ def stopwords_func(stopwords_path):
             sw.append(word)
     return sw
 
+def get_extra_stopwords(text):
+    extra_stopwords = []  # 人名列表
+    word_list = jieba.lcut(text)
+    for word in word_list:
+        if len(word)==1:  # 不加判断会爆
+            continue
+        words = pseg.lcut(word, use_paddle=True)  # paddle模式
+        print(list(words))
+        word, flag = list(words)[0]
+        if flag=='LOC' or flag == "PER":  # 这里写成LOC是地名
+            extra_stopwords.append(word)
+    extra_stopwords = list(set(extra_stopwords))
+    return extra_stopwords
 
 def load_data(corpus_path, random_status=False, random_nums=10000):
     # 加载原始数据
@@ -39,14 +53,13 @@ def load_data(corpus_path, random_status=False, random_nums=10000):
     with open(file=corpus_path, mode="r", encoding="utf8") as f:
         corpus_data = [json.loads(text) for text in f.readlines()]
     corpus_nums = len(corpus_data)
-
+    
     # 随机选取
     if random_status:
         random_data = []
         for i in random.sample(range(0, corpus_nums), random_nums):
             random_data.append(corpus_data[i])
     return random_data
-
 
 def LDA_(corpus_path, stopwords_path, save_dirpath, random_status=True, random_nums=10000):
     stopwords = stopwords_func(stopwords_path)
@@ -55,10 +68,16 @@ def LDA_(corpus_path, stopwords_path, save_dirpath, random_status=True, random_n
     title_list = []
     # 对数据进行分词处理
     jieba.load_userdict("data/userdict.txt")
+
+    extra_stopwords = []
     for text in corpus_data:
         cluster_text = " ".join(jieba.lcut(text["cluster_text"]))
         title_list.append(text["title"])
         corpus.append(cluster_text)
+        extra_stopwords.append(get_extra_stopwords(text["cluster_text"]))
+    # 额外补充地名、人名作为停用词
+    stopwords.extend(extra_stopwords)
+    
     countvector = CountVectorizer(stop_words=stopwords, max_df=0.8)
     cntTF = countvector.fit_transform(corpus)
     vocs = countvector.get_feature_names()
@@ -66,20 +85,19 @@ def LDA_(corpus_path, stopwords_path, save_dirpath, random_status=True, random_n
     # lda分析
     print("LDA模型训练")
     cpu_core_nums = multiprocessing.cpu_count()
-    lda = LatentDirichletAllocation(n_topics=THEME_NUMS, max_iter=100, learning_method="online", learning_offset=50,
-                                    n_jobs=6, random_state=0)
+    lda = LatentDirichletAllocation(n_topics=THEME_NUMS, max_iter=300, learning_method="online", learning_offset=50, n_jobs=6, random_state=9)
     docres = lda.fit_transform(cntTF)
     LDA_corpus = np.array(docres)
     print("模型训练完成！")
 
     LDA_corpus_one = np.zeros([LDA_corpus.shape[0]])
     # 对比所属两个概率的大小，确定属于的类别
-    LDA_corpus_one = np.argmax(LDA_corpus, axis=1)  # 返回沿轴axis最大值的索引，axis=1代表行；最大索引即表示最可能表示的数字是多少
+    LDA_corpus_one = np.argmax(LDA_corpus, axis=1) # 返回沿轴axis最大值的索引，axis=1代表行；最大索引即表示最可能表示的数字是多少
     # print('每个文档所属类别：', LDA_corpus_one)
 
     if not os.path.exists(save_dirpath):
         os.makedirs(save_dirpath)
-
+        
     for i in range(THEME_NUMS):
         # 选择相同主题进行存储
         theme_list = []
@@ -102,27 +120,28 @@ def LDA_(corpus_path, stopwords_path, save_dirpath, random_status=True, random_n
             # 打印每个类别前5个主题词：
             tt_dict = tt_dict[:10]
             # print('主题%d:' % (id), tt_dict)
-            save_string = '主题' + str(id) + ":" + str(tt_dict)
+            save_string = '主题'+str(id)+":"+str(tt_dict)
             f.write(json.dumps(save_string, ensure_ascii=False))
             f.write("\n")
-
+            
         f.write("\n")
         f.write("-----主题划分-----")
         f.write("\n")
-
+        
         subject = []
         for i in range(10):
-            title = "主题" + str(i)
-            subject.append({title: []})
+            title = "主题"+str(i)
+            subject.append({title:[]})
         for i, text in enumerate(LDA_corpus_one):
-            subject[text]["主题" + str(text)].append(title_list[i])
+            subject[text]["主题"+str(text)].append(title_list[i])
         for text in subject:
             f.write(json.dumps(text, ensure_ascii=False))
             f.write("\n")
-
+            
+            
 
 if __name__ == "__main__":
     corpus_path = "data/gw_cluster_head.json"
     stopwords_path = "data/stopwords.txt"
     save_dirpath = "outputs/LDA"
-    LDA_(corpus_path, stopwords_path, save_dirpath, random_status=True, random_nums=100)
+    LDA_(corpus_path, stopwords_path, save_dirpath, random_status=True, random_nums=20000)
